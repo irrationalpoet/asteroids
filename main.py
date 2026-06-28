59ac4aadd125878beb4050f96ac61b250a77e9b6
irrationalpoet <irrationalpoet@gmail.com>
2026-06-28 02:43:57 -0400

    Added clock to main loop

--- main.py
+++ main.py
@@ -3,16 +3,24 @@ from logger import log_state
 import constants
 
 
-def main():
+def main() -> None:
     pygame.init()
-    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    screen: pygame.Surface = pygame.display.set_mode(
+        (constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT)
+    )
+    clock: pygame.time.Clock = pygame.time.Clock()
+    dt: float = 0.0
+
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill("black")
+        screen.fill(color="black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+        print(dt)
+
 
 
 if __name__ == "__main__":
